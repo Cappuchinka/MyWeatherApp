1f6a5bd202770c3809447c7edbfd17ad9005d49b
Cappuchinka <volchenko.coffee@gmail.com>
2023-07-01 00:17:29 +0300

    get icon from direct URL

--- app/src/main/java/ru/kapuchinka/myweatherapp/MainActivity.kt
+++ app/src/main/java/ru/kapuchinka/myweatherapp/MainActivity.kt
@@ -3,16 +3,21 @@ package ru.kapuchinka.myweatherapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModelProvider
+import coil.compose.rememberImagePainter
+import coil.transform.CircleCropTransformation
 import ru.kapuchinka.myweatherapp.ui.theme.MyWeatherAppTheme
 import ru.kapuchinka.myweatherapp.viewmodel.WeatherViewModel
 
@@ -28,7 +33,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Voronezh", weatherViewModel)
+                    Column {
+                        Greeting("Voronezh", weatherViewModel)
+                        WeatherIcon(iconUrl = "https://openweathermap.org/img/w/01d.png", size = 64.dp)
+                    }
+
                 }
             }
         }
@@ -47,5 +56,20 @@ fun Greeting(city: String, weatherViewModel: WeatherViewModel, modifier: Modifie
     Text(
         text = "Hello $tempMax!",
         modifier = modifier
+    )
+}
+
+@Composable
+fun WeatherIcon(iconUrl: String, size: Dp) {
+    Image(
+        painter = rememberImagePainter(
+            data = iconUrl,
+            builder = {
+                transformations(CircleCropTransformation())
+                // Дополнительные настройки, если необходимо
+            }
+        ),
+        contentDescription = null,
+        modifier = Modifier.size(size)
     )
 }
