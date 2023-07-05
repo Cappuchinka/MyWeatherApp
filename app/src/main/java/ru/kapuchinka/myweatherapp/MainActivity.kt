c1f579de96ed653d8a574d50c9b702c2a6f63448
Cappuchinka <volchenko.coffee@gmail.com>
2023-07-05 23:52:27 +0300

    Add CircularProgressIndicator for Image

--- app/src/main/java/ru/kapuchinka/myweatherapp/MainActivity.kt
+++ app/src/main/java/ru/kapuchinka/myweatherapp/MainActivity.kt
@@ -1,24 +1,35 @@
 package ru.kapuchinka.myweatherapp
 
-import android.os.Bundle
 import android.Manifest
 import android.content.Context
+import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import coil.ImageLoader
+import coil.compose.AsyncImagePainter
+import coil.compose.ImagePainter
 import coil.compose.rememberImagePainter
+import coil.request.CachePolicy
+import coil.request.ImageRequest
 import coil.transform.CircleCropTransformation
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import ru.kapuchinka.myweatherapp.ui.theme.MyWeatherAppTheme
@@ -84,28 +95,35 @@ fun GetWeatherByCurrentLocation(context: Context, weatherViewModel: WeatherViewM
 
     Column {
         Text(
-            text = "Temp: $temp!",
+            text = "Temp: $temp",
             modifier = modifier
         )
         if (!weatherIconUrl.isNullOrBlank()) {
-            WeatherIcon(iconUrl = "https://openweathermap.org/img/w/${weatherIconUrl}.png", size = 64.dp)
+            WeatherIcon(iconUrl = "https://openweathermap.org/img/w/${weatherIconUrl}.png", size = 128.dp)
         }
     }
 }
 
 @Composable
 fun WeatherIcon(iconUrl: String, size: Dp) {
-    Image(
-        painter = rememberImagePainter(
+    Box(
+        modifier = Modifier
+            .height(size)
+            .width(size),
+        contentAlignment = Alignment.Center
+    ) {
+        val painter = rememberImagePainter(
             data = iconUrl,
             builder = {
-                transformations(CircleCropTransformation())
-                // Дополнительные настройки, если необходимо
+
             }
-        ),
-        contentDescription = null,
-        modifier = Modifier.size(size)
-    )
+        )
+        val painterState = painter.state
+        Image(painter = painter, contentDescription = "Weather Icon", modifier = Modifier.size(size))
+        if (painterState is AsyncImagePainter.State.Loading) {
+            CircularProgressIndicator()
+        }
+    }
 }
 
 
