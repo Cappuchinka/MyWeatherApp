b4bf4b128f9b6a8ac4edb0d1c6ab9cdd93789705
Cappuchinka <volchenko.coffee@gmail.com>
2023-07-06 21:13:19 +0300

    Change Image to AsyncImage for function WeatherIcon.

--- app/src/main/java/ru/kapuchinka/myweatherapp/MainActivity.kt
+++ app/src/main/java/ru/kapuchinka/myweatherapp/MainActivity.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -19,18 +18,18 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import coil.ImageLoader
-import coil.compose.AsyncImagePainter
-import coil.compose.ImagePainter
-import coil.compose.rememberImagePainter
-import coil.request.CachePolicy
+import coil.compose.AsyncImage
 import coil.request.ImageRequest
-import coil.transform.CircleCropTransformation
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import ru.kapuchinka.myweatherapp.ui.theme.MyWeatherAppTheme
 import ru.kapuchinka.myweatherapp.utils.permission.RequestPermission
@@ -78,7 +77,7 @@ fun GetWeatherByCity(city: String, weatherViewModel: WeatherViewModel, modifier:
             modifier = modifier
         )
         if (!weatherIconUrl.isNullOrBlank()) {
-            WeatherIcon(iconUrl = "https://openweathermap.org/img/w/${weatherIconUrl}.png", size = 64.dp)
+            WeatherIcon(iconUrl = "https://openweathermap.org/img/w/${weatherIconUrl}.png", size = 128.dp)
         }
     }
 }
@@ -106,24 +105,65 @@ fun GetWeatherByCurrentLocation(context: Context, weatherViewModel: WeatherViewM
 
 @Composable
 fun WeatherIcon(iconUrl: String, size: Dp) {
+    var isLoading by remember { mutableStateOf(true) } // Добавляем состояние для отслеживания загрузки
+
     Box(
         modifier = Modifier
             .height(size)
             .width(size),
         contentAlignment = Alignment.Center
     ) {
-        val painter = rememberImagePainter(
-            data = iconUrl,
-            builder = {
-
-            }
-        )
-        val painterState = painter.state
-        Image(painter = painter, contentDescription = "Weather Icon", modifier = Modifier.size(size))
-        if (painterState is AsyncImagePainter.State.Loading) {
+        if (isLoading) {
             CircularProgressIndicator()
         }
+        AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(iconUrl)
+                .crossfade(true)
+                .listener(
+                    onSuccess = { request, metadata ->
+                        isLoading = false // Устанавливаем isLoading в false, когда изображение загружено
+                    },
+                    onError = { request, throwable ->
+                        // Обработка ошибки, если не удалось загрузить изображение
+                    }
+                )
+                .build(),
+            contentDescription = "Weather Icon",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.size(size)
+        )
     }
 }
+
+//@Composable
+//fun LoadImageWithCache(context: Context, imageUrl: String){
+//    val imageLoader = ImageLoader.Builder(context)
+//        .memoryCachePolicy(CachePolicy.ENABLED)
+//        .memoryCache {
+//            MemoryCache.Builder(context)
+//                .maxSizePercent(0.25)
+//                .build()
+//        }
+//        .diskCache {
+//            DiskCache.Builder()
+//                .directory(context.cacheDir.resolve("image_cache"))
+//                .maxSizePercent(0.02)
+//                .build()
+//        }
+//        .build()
+//
+//    val request = remember {
+//        ImageRequest.Builder(context)
+//            .data(imageUrl)
+//            .build()
+//    }
+//
+//    val painter = rememberAsyncImagePainter(
+//        request = request,
+//        imageLoader = imageLoader,
+//
+//    )
+//}
 
 
